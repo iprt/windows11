33738f61b812102a02d0543ce4c9ce5daefb1ec2
zhuzhenjie <mimotronik@gmail.com>
2022-07-02 00:21:37 +0800

    refactor

--- src/main/java/org/iproute/windows11/controller/ComputerController.java
+++ src/main/java/org/iproute/windows11/controller/ComputerController.java
@@ -3,15 +3,13 @@ package org.iproute.windows11.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.iproute.windows11.Utils.CmdUtils;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * ComputerController
@@ -21,14 +19,8 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 @RestController
 @Slf4j
-public class ComputerController implements CommandLineRunner {
-    private ReentrantLock restartLock = new ReentrantLock(true);
-    private String uuid;
-
-    @GetMapping("/sayHello")
-    public String sayHello() {
-        return "hello world";
-    }
+public class ComputerController {
+    private ConcurrentHashMap<String, String> uuidMap = new ConcurrentHashMap<>(1);
 
     /**
      * Unlock with uuid string.
@@ -37,56 +29,31 @@ public class ComputerController implements CommandLineRunner {
      */
     @PostMapping("/uuid")
     public String unlockWithUuid() {
-        restartLock.lock();
-        try {
-            return this.uuid;
-        } catch (Exception e) {
-            return "get uuid failed";
-        } finally {
-            restartLock.unlock();
-        }
+        return uuidMap.get("uuid");
     }
 
     @PostMapping("/computer/restart")
     @SuppressWarnings("all")
-    public String restart(HttpServletRequest request) {
-        String id = request.getHeader("uuid");
-        if (StringUtils.isBlank(id) || uuid.equals(id)) {
+    public String restart(@RequestBody Req req) {
+        String uuid = req.getUuid();
+        if (StringUtils.isBlank(uuid) || !StringUtils.equals(uuid, uuidMap.get("uuid"))) {
             return "restart failed";
         }
         new Thread(() -> {
             try {
                 Thread.sleep(5 * 1000);
+                log.info("restart computer");
             } catch (InterruptedException e) {
             }
 
             CmdUtils.restartComputer();
         }).start();
-
         return "restart success";
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        restartLock.lock();
-        try {
-            this.uuid = UUID.randomUUID().toString();
-            log.info("init uuid :{}", uuid);
-        } finally {
-            restartLock.unlock();
-        }
     }
 
     @Scheduled(fixedRate = 1000L * 30)
     public void refreshId() {
-        restartLock.lock();
-        try {
-            this.uuid = UUID.randomUUID().toString();
-            log.info("refresh uuid : {}", this.uuid);
-        } finally {
-            restartLock.unlock();
-        }
+        uuidMap.put("uuid", UUID.randomUUID().toString());
     }
-
 
 }
---
+++ src/main/java/org/iproute/windows11/controller/Req.java
@@ -0,0 +1,14 @@
+package org.iproute.windows11.controller;
+
+import lombok.Data;
+
+/**
+ * Req
+ *
+ * @author zhuzhenjie
+ * @since 2022/7/2
+ */
+@Data
+public class Req {
+    private String uuid;
+}
