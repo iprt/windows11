6bb4d40d918f0251d3cdf7d2da61b1531059f747
zhuzhenjie <mimotronik@gmail.com>
2022-07-04 11:30:54 +0800

    exception handler

--- src/main/java/org/iproute/windows11/config/RestExceptionHandler.java
+++ src/main/java/org/iproute/windows11/config/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.iproute.windows11.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,8 +18,8 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public String handleException(Exception e) {
+    public ResponseEntity<Void> handleException(Exception e) {
         log.error("Exception", e);
-        return "404";
+        return ResponseEntity.status(444).build();
     }
 }
